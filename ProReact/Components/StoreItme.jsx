0c6a079c226dbaa8577390a863d20f10aea4891b
Mindi1234 <mimikenig1@gmail.com>
2025-09-21 15:38:26 +0300

    css

--- ProReact/Components/StoreItme.jsx
+++ ProReact/Components/StoreItme.jsx
@@ -1,26 +1,32 @@
-import React,{ useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { getProducts } from "../Service/service";
+import '../Css/StorageCss.css'; 
 
-const StoreItme = () =>{
+const StoreItme = () => {
+  const [items, setItems] = useState([]);
 
-    const [items , setItems] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const products = await getProducts();
+      setItems(products);
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const products = await getProducts().then();
-          setItems(products);
-        };
-        fetchData();
-      }, []);
+  return (
+    <div className="store-container">
+      <h1>products</h1>
+      <div className="grid">
+        {items.map(item => (
+          <div key={item.id} className="card">
+            <img src={item.image} alt={item.title} />
+            <div className="title">{item.title}</div>
+            <div className="price">${item.price}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    useEffect(() => {
-        console.log("Items changed:", items);
-    }, [items]);
-
-    return(
-        <>
-
-        </>
-    )
-}
 export default StoreItme;
